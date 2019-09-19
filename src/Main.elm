ace8d25826f5a8f54d60c7a9008959f21182ebea
Thomas Forgione <thomas@forgione.fr>
2019-09-19 21:24:35 +0200

    Adds search bar

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,82 @@
 module Main exposing (main, myElement, myRowOfStuff)
 
-import Element exposing (Element, alignRight, centerY, el, fill, padding, rgb255, row, spacing, text, width)
+import Browser
+import Element exposing (Element, alignRight, centerY, el, fill, height, padding, paddingXY, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import Element.Input as Input
 
 
 main =
-    Element.layout []
-        myRowOfStuff
+    Browser.sandbox { init = init, update = update, view = view }
 
 
-myRowOfStuff : Element msg
-myRowOfStuff =
-    row [ width fill, Element.alignTop, spacing 30, Background.color (rgb255 191 100 114) ]
-        [ myElement
-        , myElement
-        , el [ alignRight ] myElement
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SearchChanged text ->
+            { model | searchField = text }
+
+        SearchPressed ->
+            model
+
+
+view { searchField } =
+    Element.layoutWith { options = [ Element.focusStyle noFocusStyle ] }
+        []
+        (myRowOfStuff searchField)
+
+
+type alias Model =
+    { searchField : String
+    }
+
+
+init =
+    { searchField = ""
+    }
+
+
+type Msg
+    = SearchChanged String
+    | SearchPressed
+
+
+noFocusStyle : Element.FocusStyle
+noFocusStyle =
+    { borderColor = Nothing
+    , backgroundColor = Nothing
+    , shadow = Nothing
+    }
+
+
+searchBar : String -> Element Msg
+searchBar searchField =
+    row [ Background.color (rgb255 99 199 106) ]
+        [ Input.text [ paddingXY 10 5, Border.width 0, Border.rounded 0 ]
+            { onChange = SearchChanged
+            , placeholder = Just (Input.placeholder [] (text "Search..."))
+            , label = Input.labelHidden "Search field"
+            , text = searchField
+            }
+        , Input.button [ paddingXY 10 5 ]
+            { label = text "🔎"
+            , onPress = Just SearchPressed
+            }
         ]
 
 
-myElement : Element msg
+myRowOfStuff : String -> Element Msg
+myRowOfStuff searchField =
+    row [ width fill, Element.alignTop, padding 10, spacing 30, Background.color (rgb255 191 100 114) ]
+        [ myElement
+        , myElement
+        , el [ alignRight ] (searchBar searchField)
+        ]
+
+
+myElement : Element Msg
 myElement =
     el
         [ Background.color (rgb255 240 0 245)
